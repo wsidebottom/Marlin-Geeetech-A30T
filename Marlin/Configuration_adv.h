b50b30e6c06e1c670c9f0383f30de8f709888f2a
thomasd <thomas.dorka@gmail.com>
2022-03-19 12:40:46 +0100

    Set very high bed temp watch value to reach at least 95 degress

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -307,7 +307,7 @@
   /**
    * As described above, except for the bed (M140/M190/M303).
    */
-  #define WATCH_BED_TEMP_PERIOD               120 // Seconds
+  #define WATCH_BED_TEMP_PERIOD               240 // Seconds
   #define WATCH_BED_TEMP_INCREASE               2 // Degrees Celsius
 #endif
 
